d9d5b80fb9abc90a6bb757eae40e3b553fbd2b4b
atrox3d <atrox3d@gmail.com>
2024-05-09 10:54:55 +0200

    update options

--- options.py
+++ options.py
@@ -4,9 +4,9 @@ def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dict', action='store_true', default=True)
     parser.add_argument('-l', '--list', action='store_true', default=False)
-    parser.add_argument('-e', '--exclude', action='append', default=['.git'])
+    parser.add_argument('-e', '--exclude', action='append', default=['.git', '__pycache__'])
     parser.add_argument('-p', '--path', default='.')
-    parser.add_argument('-j', '--jsonpath', default='maze-solver.json')
+    parser.add_argument('-j', '--jsonpath', default='scan-path.json')
     
     args = parser.parse_args()
     args.dict = not args.list
