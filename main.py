68181e32032dc8d426c5b7ec2088c1feaa9be165
atrox3d <atrox3d@gmail.com>
2024-05-10 06:02:48 +0200

    rename scan to tree

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
 
 import options
-import scan
+import tree
 
 if __name__ == '__main__':
     args = options.parse()
-    scan.save_jsonpaths(args.jsonpath, args.path, *args.exclude, use_dict=args.dict)
-    print(json.dumps(scan.load_jsonpaths(args.jsonpath), indent=2))
+    tree.save_jsonpaths(args.jsonpath, args.path, *args.exclude, use_dict=args.dict)
+    print(json.dumps(tree.load_jsonpaths(args.jsonpath), indent=2))
